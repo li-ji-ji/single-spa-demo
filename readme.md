d41a5bab7950cdf014966c67512d9850e7a06916
lhj <1187382451@qq.com>
2021-02-24 12:04:08 +0800

    使用create single-spa初始化

---
+++ readme.md
@@ -0,0 +1,8 @@
+## demo
+
+- npx create-react-app
+- npm create single-aps
+- delete public/
+- root-component.js => app.js
+- add file-loader
+- npm run start:standalone
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "hzby",
+    projectName: "single-spa-demo",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    },
+  });
+};
